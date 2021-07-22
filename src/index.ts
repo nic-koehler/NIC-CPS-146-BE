7b93356ee6d31f25e6e13f9237826938bf028885
Brian Koehler <brian@koehler.ca>
2021-07-21 20:06:47 -0700

    added link to MySQL

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,83 @@ import { Firestore, Timestamp } from '@google-cloud/firestore';
 import querystring from 'querystring';
 import axios from 'axios';
 import crypto from 'crypto';
+import mysql from 'promise-mysql';
+
+// [START cloud_sql_mysql_mysql_create_socket]
+const createUnixSocketPool = async (config: any) => {
+  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+
+  // Establish a connection to the database
+  return await mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    // If connecting via unix domain socket, specify the path
+    socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+    // Specify additional properties here.
+    ...config,
+  });
+};
+// [END cloud_sql_mysql_mysql_create_socket]
+
+const createPool = async () => {
+  const config = {
+    // [START cloud_sql_mysql_mysql_limit]
+    // 'connectionLimit' is the maximum number of connections the pool is allowed
+    // to keep at once.
+    connectionLimit: 5,
+    // [END cloud_sql_mysql_mysql_limit]
+
+    // [START cloud_sql_mysql_mysql_timeout]
+    // 'connectTimeout' is the maximum number of milliseconds before a timeout
+    // occurs during the initial connection to the database.
+    connectTimeout: 10000, // 10 seconds
+    // 'acquireTimeout' is the maximum number of milliseconds to wait when
+    // checking out a connection from the pool before a timeout error occurs.
+    acquireTimeout: 10000, // 10 seconds
+    // 'waitForConnections' determines the pool's action when no connections are
+    // free. If true, the request will queued and a connection will be presented
+    // when ready. If false, the pool will call back with an error.
+    waitForConnections: true, // Default: true
+    // 'queueLimit' is the maximum number of requests for connections the pool
+    // will queue at once before returning an error. If 0, there is no limit.
+    queueLimit: 0, // Default: 0
+    // [END cloud_sql_mysql_mysql_timeout]
+
+    // [START cloud_sql_mysql_mysql_backoff]
+    // The mysql module automatically uses exponential delays between failed
+    // connection attempts.
+    // [END cloud_sql_mysql_mysql_backoff]
+  };
+  return await createUnixSocketPool(config);
+
+};
+
+const ensureSchema = async (pool: any) => {
+  // Wait for tables to be created (if they don't already exist).
+  // await pool.query(
+  //   `CREATE TABLE IF NOT EXISTS votes
+  //     ( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL,
+  //     candidate CHAR(6) NOT NULL, PRIMARY KEY (vote_id) );`
+  // );
+  console.log("Ensured that table 'votes' exists");
+};
+
+const createPoolAndEnsureSchema = async () =>
+  await createPool()
+    .then(async pool => {
+      await ensureSchema(pool);
+      return pool;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+
+// Set up a variable to hold our connection pool. It would be safe to
+// initialize this right away, but we defer its instantiation to ease
+// testing different configurations.
+let pool: any;
 
 const db = new Firestore({
 //  projectId: 'cps-146'
@@ -94,6 +171,12 @@ export const nicMySQL: HttpFunction = async (req, res) => {
             account
           } );
           message = `Added new account: ${account}`;
+          pool = pool || (await createPoolAndEnsureSchema());
+          const usersQuery = pool.query(
+            'select Host,User from user'
+          );
+          const users = await usersQuery;
+          console.log( users );
         }
       } else {
         message = 'Invalid or expired link.'
